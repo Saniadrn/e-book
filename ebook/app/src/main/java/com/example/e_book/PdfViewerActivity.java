da549e5cb5c76d5351a7248078bb4ba5b6443ff0
dorsa <dorsa.faham.nanadi.df@gmail.com>
2025-04-06 17:23:22 +0200

    added comments pdf viewer

--- ebook/app/src/main/java/com/example/e_book/PdfViewerActivity.java
+++ ebook/app/src/main/java/com/example/e_book/PdfViewerActivity.java
@@ -50,7 +50,7 @@ public class PdfViewerActivity extends AppCompatActivity {
         Log.d("PDF_URL","link daryaft shod"+pdfUrl);//ثبت یک پیغام در Logcat به منظور اشکال زدایی
         new DownloadPdfTask().execute(pdfUrl);
         //new DownloadPdfTask()  --> مسِئول دانلود فایل پی دی اف از URL
-        //.execute(pdfUrl)       --> مسئول ارسال pdfurl به عنوان ارگومان
+        //.execute(pdfUrl)       --> عملیات دانلود PDF در پس‌زمینه اجرا شود.
 
         //سه خط کد با هم یک جریان کلی زیر را انجام می‌دهند:
         //۱. یک URL برای یک فایل PDF از intent دریافت می‌شود.
@@ -62,27 +62,43 @@ public class PdfViewerActivity extends AppCompatActivity {
     }
 
     private class DownloadPdfTask extends AsyncTask<String, Void, File> {
+//       AsyncTask    --> این کلاس مسئول دانلود فایل PDF در یک ترد پس‌زمینه است تا رابط کاربری بلاک نشود.
 
         @Override
         protected File doInBackground(String... strings) {
+//            تد اصلی که در ترد پس‌زمینه اجرا می‌شود
             Log.d("PDF_DOWNLOAD","download shoro shod");
             try {
+//                کتابخانه OkHttp برای درخواست‌های شبکه‌ای
                 OkHttpClient client = new OkHttpClient();
                 Request request = new Request.Builder().url(strings[0]).build();
+//                اینجا strings[0] همون pdfUrl است
+//                Request.Builder --> ریکوست درست میکند
                 Response response = client.newCall(request).execute();
+//               درخواست را ارسال می‌کنه و پاسخ سرور در شی Response قرار می‌گیرد
                 if(!response.isSuccessful()){
                     Log.e("PDF_DOWNLOAD","daryaft file"+response.code());
                 }
                 InputStream inputStream = response.body().byteStream();
+//                از بدنه پاسخ (response.body()) استریم می‌گیرد (byteStream()). این استریم داده‌های PDF را برمی‌گرداند.
+//                استریم می‌گیرد» یعنی که به جای اینکه کل فایل را یک‌باره در حافظه بگیریم
+//                می‌تونیم مرحله به مرحله و به‌صورت جریان داده و داده را بایت به بایت یا در بافرهای کوچک از سرور بخوانیم
                 File file = new File(getFilesDir(), "temp.pdf");
+//             ساخت   فایلی به نام temp.pdf در مسیر داخلی برنامه پوشه getFilesDi
                 FileOutputStream outputStream = new FileOutputStream(file);
+//                یک FileOutputStream برای نوشتن بایت‌ها در فایل باز می‌کنیم.
                 byte[] buffer = new byte[1024];
                 int length;
                 while ((length = inputStream.read(buffer)) != -1) {
                     outputStream.write(buffer, 0, length);
                 }
+//                ه کمک inputStream.read(buffer), داده‌ها را از پاسخ شبکه می‌خوانیم و تا زمانی که مقدار بازگشتی
+//                از متد read منفی نباشد ( -1 به معنی انتهای فایل است)، آن را در outputStream می‌نویسیم.
+
+//                در اصل این حلقه کل PDF را از سرور می‌گیرد و به فایل temp.pdf در حافظه داخلی منتقل می‌کند.
                 outputStream.close();
                 inputStream.close();
+//                استریم‌ها را می‌بندیم تا منابع آزاد شوند.
                 return file;
             } catch (Exception e) {
                 e.printStackTrace();
@@ -93,6 +109,7 @@ public class PdfViewerActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(File file) {
+//            ین متد روی ترد اصلی (UI thread) صدا زده می‌شود
             progressBar.setVisibility(View.GONE);
             if(file!=null){
                 pdfView.fromFile(file).load();
